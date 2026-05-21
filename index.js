774784c9c18bed4656c2aa8f12af7beec9a78956
masi_dev <maxitodeveloper@gmail.com>
2026-05-20 22:38:33 -0300

    REINICIO

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-function calcularImpuestos(subtotal, porcentajeImpuesto) {
-    return subtotal * (porcentajeImpuesto / 100);
-}
-
-console.log("Impuestos:", calcularImpuestos(22000, 21));
+console.log("Proyecto Tienda de Focos iniciado");
